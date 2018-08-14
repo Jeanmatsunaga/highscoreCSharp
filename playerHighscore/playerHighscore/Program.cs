d0a146688f74d1b2d275046b6b626cb9500304b5
Jean Matsunaga <inar1@outlook.com>
2018-08-13 22:53:57 -0300

    The code is working, check the player and current highscore.

--- playerHighscore/playerHighscore/Program.cs
+++ playerHighscore/playerHighscore/Program.cs
@@ -8,11 +8,47 @@ namespace playerHighscore
 {
     class Program
     {
+        static string highscorePlayer;
+        static int highscore;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Exemplo de uso do GitHub.");
-            Console.WriteLine("It Works!");
+            int score;
+            string playerName;
+
+            // Insert the actual Highscore;
+            Console.WriteLine("Highscore:");
+            highscore = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Highscore Name:");
+            highscorePlayer = Console.ReadLine();
+
+            // Insert your personal Score;
+            Console.WriteLine("Please, enter your score:");
+            score = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please, enter your name:");
+            playerName = Console.ReadLine();
+
+            checkHighscore(score, playerName);
             Console.Read();
+
+        }
+
+        static void checkHighscore(int score, string playerName)
+        {
+            if(score > highscore)
+            {
+                highscorePlayer = playerName;
+                highscore = score;
+
+                Console.WriteLine("New highscore is " + highscore + ".");
+                Console.WriteLine("New highscore holder is " + highscorePlayer + ".");
+            } else
+            {
+                Console.WriteLine("The old highscore of " + highscore + " could not be broken and is still held by " + highscorePlayer + ".");
+            }
+
         }
     }
 }
