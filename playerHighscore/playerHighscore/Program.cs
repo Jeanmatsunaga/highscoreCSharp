0068a366835aa4ef9d182397eff4e02c65ac361a
Jean Matsunaga <inar1@outlook.com>
2018-08-13 22:19:02 -0300

    Commit Inicial;

---
+++ playerHighscore/playerHighscore/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace playerHighscore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exemplo de uso do GitHub.");
+            Console.Read();
+        }
+    }
+}
